74ea9333fd62c0b590f04be71dfff3b44b9d0b6f
Siyuan Zhou <visualzhou@gmail.com>
2013-05-08 23:17:14 -0400

    Add cluster params for perl script and cpp main program.

--- src/tet/driv_rgf.cpp
+++ src/tet/driv_rgf.cpp
@@ -1,5 +1,5 @@
 /* * * * *
- *  driv_rgf.cpp 
+ *  driv_rgf.cpp
  *  Copyright (C) 2011, 2012 Rie Johnson
  *
  *  This program is free software: you can redistribute it and/or modify
@@ -17,94 +17,124 @@
  * * * * */
 
 #define _AZ_MAIN_
+
+#include <map>
 #include "AzUtil.hpp"
 #include "AzTETmain.hpp"
 #include "AzRgfTrainerSel.hpp"
 #include "AzTET_Eval_Dflt.hpp"
 #include "AzHelp.hpp"
 #include "accumulate.h"
+#include "utils.h"
+
+using std::map;
+using std::string;
 
 /*-----------------------------------------------------------------*/
 void help(int argc, const char *argv[])
 {
-  cout << "Arguments: action  parameters" <<endl; 
-  cout << "   action: "<<kw_train<<"|"<<kw_predict<<"|"<<kw_train_test<<"|"<<kw_train_predict<<"|"<<kw_features<<endl; 
-  AzHelp h(log_out); 
-  h.set_indent(11); 
-  h.set_kw_width(17); 
-  AzBytArr s_kw, s_desc; 
-  s_kw.reset(kw_train); s_kw.c("      ..."); s_desc.reset(help_train); 
-  h.item_noquotes(s_kw.c_str(), s_desc.c_str()); 
-  s_kw.reset(kw_predict); s_kw.c("    ..."); s_desc.reset(help_predict); 
-  h.item_noquotes(s_kw.c_str(), s_desc.c_str()); 
-  s_kw.reset(kw_train_test); s_kw.c(" ..."); s_desc.reset(help_train_test); 
-  h.item_noquotes(s_kw.c_str(), s_desc.c_str()); 
-  s_kw.reset(kw_train_predict); s_kw.c(" ..."); s_desc.reset(help_train_predict); 
-  h.item_noquotes(s_kw.c_str(), s_desc.c_str()); 
-  s_kw.reset(kw_features); s_kw.c(" ..."); s_desc.reset(help_features); 
-  h.item_noquotes(s_kw.c_str(), s_desc.c_str()); 
-  cout << endl; 
-  cout << "To get help on parameters, enter "<<argv[0]<<" action."<<endl; 
-  cout << "For example:  "<<argv[0]<<" "<<kw_train_test<<endl; 
-  cout << "              "<<argv[0]<<" "<<kw_train     <<endl; 
+  cout << "Arguments: action  parameters" <<endl;
+  cout << "   action: "<<kw_train<<"|"<<kw_predict<<"|"<<kw_train_test<<"|"<<kw_train_predict<<"|"<<kw_features<<endl;
+  AzHelp h(log_out);
+  h.set_indent(11);
+  h.set_kw_width(17);
+  AzBytArr s_kw, s_desc;
+  s_kw.reset(kw_train); s_kw.c("      ..."); s_desc.reset(help_train);
+  h.item_noquotes(s_kw.c_str(), s_desc.c_str());
+  s_kw.reset(kw_predict); s_kw.c("    ..."); s_desc.reset(help_predict);
+  h.item_noquotes(s_kw.c_str(), s_desc.c_str());
+  s_kw.reset(kw_train_test); s_kw.c(" ..."); s_desc.reset(help_train_test);
+  h.item_noquotes(s_kw.c_str(), s_desc.c_str());
+  s_kw.reset(kw_train_predict); s_kw.c(" ..."); s_desc.reset(help_train_predict);
+  h.item_noquotes(s_kw.c_str(), s_desc.c_str());
+  s_kw.reset(kw_features); s_kw.c(" ..."); s_desc.reset(help_features);
+  h.item_noquotes(s_kw.c_str(), s_desc.c_str());
+  cout << endl;
+  cout << "To get help on parameters, enter "<<argv[0]<<" action."<<endl;
+  cout << "For example:  "<<argv[0]<<" "<<kw_train_test<<endl;
+  cout << "              "<<argv[0]<<" "<<kw_train     <<endl;
+}
+
+void parse_args(int argc, const char* argv[], map<string, string>& arg_map) {
+    string prefix("--");
+    for (int i = 0; i < argc-1; ++i)
+    {
+        string arg(argv[i]);
+        if (arg.compare(0, prefix.size(), prefix) == 0) {
+            string arg_value(argv[i+1]);
+            arg_map[arg.substr(2)] = arg_value;
+            ++i;
+        }
+    }
 }
 
 /*******************************************************************/
 /*     main                                                        */
 /*******************************************************************/
-int main(int argc, const char *argv[]) 
+int main(int argc, const char *argv[])
 {
-  AzException *stat = NULL; 
+  AzException *stat = NULL;
 
   if (argc < 2) {
-    help(argc, argv); 
-    return -1; 
+    help(argc, argv);
+    return -1;
   }
-  string master_location="localhost";
-  int node = 0;
-  int unique_id = 1234;
-  int total = 1;
-  Hadoop::Cluster::init_cluster(node, total, unique_id, master_location);
-  const char *action = argv[1]; 
 
-  AzRgfTrainerSel alg_sel; 
-  AzTET_Eval_Dflt eval; 
-  AzTETmain driver(&alg_sel, &eval); 
+  // Register to cluster if cluster parameters are given.
+  map<string, string> arg_map;
+  parse_args(argc, argv, arg_map);
+  if (arg_map.size() > 0) {
+    // cluster parameters are given.
+    verify(arg_map.count("master") > 0);
+    verify(arg_map.count("node_id") > 0);
+    verify(arg_map.count("total") > 0);
+    verify(arg_map.count("unique_id") > 0);
+    int node = atoi(arg_map["node_id"].c_str());
+    int unique_id = atoi(arg_map["unique_id"].c_str());
+    int total = atoi(arg_map["total"].c_str());
+    Hadoop::Cluster::init_cluster(node, total, unique_id, arg_map["master"]);
+  }
+
+  const char *action = argv[1];
+
+  AzRgfTrainerSel alg_sel;
+  AzTET_Eval_Dflt eval;
+  AzTETmain driver(&alg_sel, &eval);
   try {
-    Az_check_system_(); 
+    Az_check_system_();
 
     if (strcmp(action, kw_train) == 0) {
-      driver.train(argv, argc); 
+      driver.train(argv, argc);
     }
     else if (strcmp(action, kw_predict) == 0) {
-      driver.predict_single(argv, argc); 
+      driver.predict_single(argv, argc);
     }
     else if (strcmp(action, kw_train_test) == 0) {
-      driver.train_test(argv, argc); 
+      driver.train_test(argv, argc);
     }
     else if (strcmp(action, kw_train_predict) == 0) {
-      driver.train_predict(argv, argc); 
+      driver.train_predict(argv, argc);
     }
     else if (strcmp(action, kw_batch_predict) == 0) {
-      driver.batch_predict(argv, argc); 
+      driver.batch_predict(argv, argc);
     }
     else if (strcmp(action, kw_features) == 0) {
-      driver.features(argv, argc); 
+      driver.features(argv, argc);
     }
     else {
-      help(argc, argv); 
-      return -1; 
+      help(argc, argv);
+      return -1;
     }
   }
   catch (AzException *e) {
-    stat = e; 
+    stat = e;
   }
 
   if (stat != NULL) {
-    cout << stat->getMessage() << endl; 
-    return -1; 
+    cout << stat->getMessage() << endl;
+    return -1;
   }
 
-  return 0; 
+  return 0;
 }
 
--- test/call_exe.pl
+++ test/call_exe.pl
@@ -1,175 +1,183 @@
 
-  use strict 'vars'; 
+  use strict 'vars';
 
-  my $dlm = ','; 
-  my $inp_ext = '.inp'; 
+  my $dlm = ',';
+  my $inp_ext = '.inp';
 
-  my $arg_num = $#ARGV + 1; 
-  if ($arg_num != 3) {
-    print "Arguments: exe train|predict|train_test cfg_fn\n"; 
-    print "   exe   : Name of the executable.  Typically, ../bin/rgf \n"; 
-    print "   train|predict|train_test \n"; 
-    print "           train      ... Train and save models to files.  \n"; 
-    print "           predict    ... Apply a model to new data. \n"; 
-    print "           train_test ... Train and test models in one call. \n"; 
-    print "   cfg_fn: Path to a configuration file without extension; e.g., sample/train \n"; 
-    print "           The file extension should be \".inp\"\n";   
-    print "           For example, see sample/train.inp and sample/predict.inp.\n"; 
-    print "\n"; 
-    print "           To get help on the parameters that can be used in configuration\n"; 
-    print "           files, call the executable with train|predict|train_test, e.g.,\n"; 
-    print "\n"; 
-    print "                           ..\\bin\\rgf  train \n"; 
-    print "                           ../bin/rgf  predict \n"; 
-    exit 1; 
+  my $arg_num = $#ARGV + 1;
+  if ($arg_num < 3) {
+    print "Arguments: exe train|predict|train_test cfg_fn\n";
+    print "   exe   : Name of the executable.  Typically, ../bin/rgf \n";
+    print "   train|predict|train_test \n";
+    print "           train      ... Train and save models to files.  \n";
+    print "           predict    ... Apply a model to new data. \n";
+    print "           train_test ... Train and test models in one call. \n";
+    print "   cfg_fn: Path to a configuration file without extension; e.g., sample/train \n";
+    print "           The file extension should be \".inp\"\n";
+    print "           For example, see sample/train.inp and sample/predict.inp.\n";
+    print "\n";
+    print "           To get help on the parameters that can be used in configuration\n";
+    print "           files, call the executable with train|predict|train_test, e.g.,\n";
+    print "\n";
+    print "                           ..\\bin\\rgf  train \n";
+    print "                           ../bin/rgf  predict \n";
+    exit 1;
   }
 
-  my $argx = 0; 
-  my $exe = $ARGV[$argx++]; 
-  my $action = $ARGV[$argx++]; 
-  my $inp_fn = $ARGV[$argx++]; 
+  my $argx = 0;
+  my $exe = $ARGV[$argx++];
+  my $action = $ARGV[$argx++];
+  my $inp_fn = $ARGV[$argx++];
+  my $rest_params = "";
+  # perl call_exe.pl master unique_id total node_id
+  if ($arg_num > 3) {
+    my $master = $ARGV[$argx++];
+    my $unique_id = $ARGV[$argx++];
+    my $total = $ARGV[$argx++];
+    my $node_id = $ARGV[$argx++];
+    $rest_params = "--master $master --unique_id $unique_id --total $total --node_id $node_id";
+  }
 
-  $inp_fn .= $inp_ext; 
+  $inp_fn .= $inp_ext;
 
-  my @list = &readList_inc($inp_fn); 
-  my $num = $#list + 1; 
+  my @list = &readList_inc($inp_fn);
+  my $num = $#list + 1;
 
-  my $config = ""; 
+  my $config = "";
 
-  my $repeat_num = 0; 
-  my(@repeat); 
-  my $ix; 
+  my $repeat_num = 0;
+  my(@repeat);
+  my $ix;
   for ($ix = 0; $ix < $num; ++$ix) {
-    my $kw = ""; 
+    my $kw = "";
     if ($list[$ix] =~ /^\@(.*)$/) {
-      $repeat[$repeat_num++] = $1; 
+      $repeat[$repeat_num++] = $1;
     }
     else {
-      &concatKwVal($list[$ix]); 
+      &concatKwVal($list[$ix]);
     }
   }
-
 
   if ($repeat_num == 0) {
-    my $cmd = "$exe $action $config"; 
-    print "$cmd\n"; 
-    system($cmd); 
+    my $cmd = "$exe $action $config $rest_params";
+    print "$cmd\n";
+    system($cmd);
   }
   else {
-    my $rx; 
+    my $rx;
     for ($rx = 0; $rx < $repeat_num; ++$rx) {
-      my $my_config = $repeat[$rx] . ',' . $config; 
+      my $my_config = $repeat[$rx] . ',' . $config;
 
-      my $cmd = "$exe $action $my_config"; 
-      print "$cmd\n"; 
-      system($cmd); 
-      my $ret = $?; 
+      my $cmd = "$exe $action $my_config $rest_params";
+      print "$cmd\n";
+      system($cmd);
+      my $ret = $?;
       if ($ret != 0) {
-        print "system() failed; exit call_exe.pl\n"; 
-        exit 1; 
+        print "system() failed; exit call_exe.pl\n";
+        exit 1;
       }
     }
   }
 
-  exit 0; 
+  exit 0;
 
 ##-----------------------------------
-##  if there is a line "#include filename" ... 
+##  if there is a line "#include filename" ...
 sub readList_inc {
-  my($fn) = @_; 
-  my(@out); 
+  my($fn) = @_;
+  my(@out);
 
-  my @list = &readList($fn); 
+  my @list = &readList($fn);
 
-  my $num = $#list + 1; 
-  my $out_ix = 0; 
-  my $ix; 
+  my $num = $#list + 1;
+  my $out_ix = 0;
+  my $ix;
   for ($ix = 0; $ix < $num; ++$ix) {
     if ($list[$ix] =~ /^\include\s+(\S+)$/) {
-      my $inc_fn = $1; 
-      my(@list1); 
-      @list1 = &readList($inc_fn); 
-      my $num1 = $#list1 + 1; 
-      my $jx; 
+      my $inc_fn = $1;
+      my(@list1);
+      @list1 = &readList($inc_fn);
+      my $num1 = $#list1 + 1;
+      my $jx;
       for ($jx = 0; $jx < $num1; ++$jx) {
         if ($list1[$jx] =~ /^include/) {
-          print "Can't nest include: $inc_fn\n"; 
-          exit 1; 
+          print "Can't nest include: $inc_fn\n";
+          exit 1;
         }
-        $out[$out_ix++] = &checkInputLine($list1[$jx]); 
+        $out[$out_ix++] = &checkInputLine($list1[$jx]);
       }
     }
     else {
-      $out[$out_ix++] = &checkInputLine($list[$ix]); 
+      $out[$out_ix++] = &checkInputLine($list[$ix]);
     }
   }
-  return @out; 
+  return @out;
 }
 
 ##------
 sub concatKwVal {
-  my($inp) = @_; 
+  my($inp) = @_;
   if ($inp =~ /\S/) {
     if ($config ne "") {
-      $config .= $dlm; 
+      $config .= $dlm;
     }
-    $config .= $inp; 
+    $config .= $inp;
   }
 }
 
 #####
 sub readList {
-  my($lst_fn) = @_; 
-  my(@item);   
+  my($lst_fn) = @_;
+  my(@item);
 
   if (!open(LST, "$lst_fn")) {
-    print "Can't open $lst_fn\n"; 
-    exit 1; 
+    print "Can't open $lst_fn\n";
+    exit 1;
   }
 
-  my $num = 0; 
+  my $num = 0;
   while(<LST>) {
-    my $line = $_; 
-    chomp $line; 
+    my $line = $_;
+    chomp $line;
     if ($line !~ /^\s*\#/) {
-      $line = &strip($line); 
-      $item[$num++] = $line; 
+      $line = &strip($line);
+      $item[$num++] = $line;
     }
   }
 
-  close(LST); 
-  return @item; 
+  close(LST);
+  return @item;
 }
 
 #####
 sub strip {
-  my($inp) = @_; 
-  my $out = $inp; 
+  my($inp) = @_;
+  my $out = $inp;
   if ($inp =~ /^\s*(\S.*\S)\s*$/) {
-    $out = $1; 
+    $out = $1;
   }
   elsif ($inp =~ /^\s*(\S)\s*$/) {
-    $out = $1; 
+    $out = $1;
   }
-  return $out; 
+  return $out;
 }
 
 #####
 sub checkInputLine {
-  my($inp) = @_; 
+  my($inp) = @_;
 
-  my $param = $inp; 
+  my $param = $inp;
   if ($param =~ /^(\S+)\s+(\S+)/) {
-    $param = $1; 
-    my $comment = $2; 
+    $param = $1;
+    my $comment = $2;
     if ($comment !~ /^\#/) {
-      print "No space is allowed in the parameter.  Comments should start with \#\n"; 
-      print "Error in: [$inp]\n"; 
-      exit; 
+      print "No space is allowed in the parameter.  Comments should start with \#\n";
+      print "Error in: [$inp]\n";
+      exit;
     }
   }
   if ($param =~ /^([^\#]+)\#/) {
-    $param = $1; 
+    $param = $1;
   }
-  return $param; 
+  return $param;
 }
