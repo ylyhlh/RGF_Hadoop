0f0a7d57f121e0926fc51484e19720fe8507d51d
Siyuan Zhou <visualzhou@gmail.com>
2013-05-09 19:46:47 -0400

    Use accumulate in findsplit.

--- src/tet/AzFindSplit.cpp
+++ src/tet/AzFindSplit.cpp
@@ -1,5 +1,5 @@
 /* * * * *
- *  AzFindSplit.cpp 
+ *  AzFindSplit.cpp
  *  Copyright (C) 2011, 2012 Rie Johnson
  *
  *  This program is free software: you can redistribute it and/or modify
@@ -17,54 +17,54 @@
  * * * * */
 
 #include "AzFindSplit.hpp"
-//#include "accumulate.h"
+#include "accumulate.h"
 
 /*--------------------------------------------------------*/
-void AzFindSplit::_begin(const AzTrTree_ReadOnly *inp_tree, 
-                         const AzDataForTrTree *inp_data, 
-                         const AzTrTtarget *inp_target, 
+void AzFindSplit::_begin(const AzTrTree_ReadOnly *inp_tree,
+                         const AzDataForTrTree *inp_data,
+                         const AzTrTtarget *inp_target,
                          int inp_min_size)
 {
-  tree = inp_tree; 
-  target = inp_target; 
-  min_size = inp_min_size; 
-  data = inp_data; 
+  tree = inp_tree;
+  target = inp_target;
+  min_size = inp_min_size;
+  data = inp_data;
 }
 
 /*-----Find best split for given tree node--------------------------------------*/
-void AzFindSplit::_findBestSplit(int nx, 
+void AzFindSplit::_findBestSplit(int nx,
                                  /*---  output  ---*/
                                  AzTrTsplit *best_split)
 {
-  const char *eyec = "AzFindSplit::_findBestSplit"; 
+  const char *eyec = "AzFindSplit::_findBestSplit";
 
   if (tree == NULL || target == NULL || data == NULL) {
-    throw new AzException(eyec, "information is not set"); 
+    throw new AzException(eyec, "information is not set");
   }
 
   /* @data indexes belonging to this node */
-  const int *dxs = tree->node(nx)->data_indexes(); 
+  const int *dxs = tree->node(nx)->data_indexes();
   //@the number of examples arrived this node.
-  const int dxs_num = tree->node(nx)->dxs_num; 
+  const int dxs_num = tree->node(nx)->dxs_num;
 
   //@return the sorted_arr belongs to this node(nx), if there is no, then get from data?
-  const AzSortedFeatArr *sorted_arr = tree->sorted_array(nx, data); 
+  const AzSortedFeatArr *sorted_arr = tree->sorted_array(nx, data);
   if (sorted_arr == NULL) {
-    throw new AzException(eyec, "No sorted array?!"); 
+    throw new AzException(eyec, "No sorted array?!");
   }
 
   /*@An object to record the stats*/
-  Az_forFindSplit total; 
+  Az_forFindSplit total;
   total.wy_sum = target->getTarDwSum(dxs, dxs_num);//@The sum of v_y in parent node
   total.w_sum = target->getDwSum(dxs, dxs_num); //@The number of v_y in parent node
 
   /*---  go through features to find the best split  ---*/
-  int feat_num = data->featNum(); 
-  const int *fxs = NULL; 
+  int feat_num = data->featNum();
+  const int *fxs = NULL;
   if (ia_fx != NULL) {
-    fxs = ia_fx->point(&feat_num); 
+    fxs = ia_fx->point(&feat_num);
   }
-  int ix; 
+  int ix;
 
   int split_points_num = 10;
   double *split_points = new double[split_points_num];
@@ -72,36 +72,36 @@ void AzFindSplit::_findBestSplit(int nx,
 
   for (ix = 0; ix < feat_num; ++ix) {
     int fx = ix; //@ the index of feature
-    if (fxs != NULL) fx = fxs[ix]; 
+    if (fxs != NULL) fx = fxs[ix];
 
-    AzSortedFeatWork tmp; 
+    AzSortedFeatWork tmp;
     const AzSortedFeat *sorted = sorted_arr->sorted(fx); //@just return the fx-th AzSortedFeat in arr
     if (sorted == NULL) { /* This happens only with Thrift or warm-start */
-      const AzSortedFeat *my_sorted = sorted_arr->sorted(data->sorted_array(), fx, &tmp); 
+      const AzSortedFeat *my_sorted = sorted_arr->sorted(data->sorted_array(), fx, &tmp);
       if (my_sorted->dataNum() != dxs_num) {
-        throw new AzException(eyec, "conflict in #data"); 
+        throw new AzException(eyec, "conflict in #data");
       }
-      loop(best_split, fx, my_sorted, dxs_num, &total); 
+      loop(best_split, fx, my_sorted, dxs_num, &total);
     }
     else {
       //std::cout<<"TEST LOOP"<<std::endl;
       ///*
 #if 1
-      
+
       pick_split_points(split_points_num,
                        sorted,
                        split_points);//@@allreduce this array please
-      //accumulate_avg(split_points, split_points_num);
+      Hadoop::accumulate_avg(split_points, split_points_num);
       loop_on_given_points(best_split, fx, sorted, dxs_num, &total, split_points, split_points_num,info);
-      
-      double bestP[2] = {0,0}; 
 
-      for (int split_index = 0; split_index < split_points_num; split_index++) { 
-        
+      double bestP[2] = {0,0};
+
+      for (int split_index = 0; split_index < split_points_num; split_index++) {
+
         double gain = evalSplit(&(info[split_index*2]), bestP); //@@@we should rewrite this to make something allreduce
         if (gain > best_split->gain) {
-          best_split->reset_values(fx, split_points[split_index] , gain, 
-                            bestP[0], bestP[1]); 
+          best_split->reset_values(fx, split_points[split_index] , gain,
+                            bestP[0], bestP[1]);
         }
       }
 
@@ -114,7 +114,7 @@ void AzFindSplit::_findBestSplit(int nx,
   //@get the feature description from data to best_split
   if (best_split->fx >= 0) {
     if (!dmp_out.isNull()) {
-      data->featInfo()->desc(best_split->fx, &best_split->str_desc); 
+      data->featInfo()->desc(best_split->fx, &best_split->str_desc);
     }
   }
   delete[] split_points;
@@ -126,34 +126,34 @@ double AzFindSplit::evalSplit(const Az_forFindSplit i[2],
                               double bestP[2])
  const
 {
-  double gain = 0; 
-  gain += getBestGain(i[0].w_sum, i[0].wy_sum, &bestP[0]); 
-  gain += getBestGain(i[1].w_sum, i[1].wy_sum, &bestP[1]); 
-  return gain; 
+  double gain = 0;
+  gain += getBestGain(i[0].w_sum, i[0].wy_sum, &bestP[0]);
+  gain += getBestGain(i[1].w_sum, i[1].wy_sum, &bestP[1]);
+  return gain;
 }
 
 /*------@Find best threshold for given fx @@@@@we should rewrite this------------------------------*/
-void AzFindSplit::loop(AzTrTsplit *best_split, 
+void AzFindSplit::loop(AzTrTsplit *best_split,
                        int fx, /* feature# */
-                       const AzSortedFeat *sorted, 
-                       int total_size, 
+                       const AzSortedFeat *sorted,
+                       int total_size,
                        const Az_forFindSplit *total)
 {
   /*---  first everyone is in GT(LE)  ---*/
   /*---  move the smallest(largest) ones from GT(LE) to LE(GT)  ---*/
-  const char *eyec = "AzFindSplit::loop"; 
-  int dest_size = 0; 
+  const char *eyec = "AzFindSplit::loop";
+  int dest_size = 0;
   Az_forFindSplit i[2];
-  Az_forFindSplit *src = &i[1], *dest = &i[0]; 
-  double bestP[2] = {0,0}; 
-  int le_idx, gt_idx; 
+  Az_forFindSplit *src = &i[1], *dest = &i[0];
+  double bestP[2] = {0,0};
+  int le_idx, gt_idx;
   if (sorted->isForward()) {
-    le_idx = 0; 
-    gt_idx = 1; 
+    le_idx = 0;
+    gt_idx = 1;
   }
   else {
-    le_idx = 1; 
-    gt_idx = 0; 
+    le_idx = 1;
+    gt_idx = 0;
   }
 
   AzCursor cursor; //@ A class can ++ --
@@ -162,49 +162,49 @@ void AzFindSplit::loop(AzTrTsplit *best_split,
   for ( ; ; ) {
     double value; //@ The value of this threshold
     int index_num; //@ The number of exampls between two values
-    const int *index = NULL; 
+    const int *index = NULL;
     index = sorted->next(cursor, &value, &index_num); //@@@@@we should rewrite this
-    if (index == NULL) break; 
-    dest_size += index_num;  
+    if (index == NULL) break;
+    dest_size += index_num;
     if (dest_size >= total_size) {
       break; /* don't allow all vs nothing */
     }
-    
+
 
     const double *tarDw = target->tarDw_arr(); //@we can look this as target
     const double *dw = target->dw_arr(); //@we can look this as 1
-    double wy_sum_move = 0, w_sum_move = 0; 
-    int ix; 
+    double wy_sum_move = 0, w_sum_move = 0;
+    int ix;
     for (ix = 0; ix < index_num; ++ix) {
-      int dx = index[ix]; 
-      wy_sum_move += tarDw[dx]; 
-      w_sum_move += dw[dx]; 
+      int dx = index[ix];
+      wy_sum_move += tarDw[dx];
+      w_sum_move += dw[dx];
     }
     dest->wy_sum += wy_sum_move; //@@@@@@@@Here here allreduce
-    dest->w_sum += w_sum_move; 
+    dest->w_sum += w_sum_move;
 
     if (min_size > 0) {
       if (dest_size < min_size) {
-        continue; 
+        continue;
       }
       if (total_size - dest_size < min_size) {
-        break; 
+        break;
       }
     }
 
-    src->wy_sum = total->wy_sum - dest->wy_sum; 
-    src->w_sum  = total->w_sum  - dest->w_sum; 
+    src->wy_sum = total->wy_sum - dest->wy_sum;
+    src->w_sum  = total->w_sum  - dest->w_sum;
 
     double gain = evalSplit(i, bestP); //@@@we should rewrite this to make something allreduce
-#if 0 
+#if 0
     best_split->keep_if_good(fx, value, gain,
-                        bestP[le_idx], bestP[gt_idx]); 
+                        bestP[le_idx], bestP[gt_idx]);
 #else
     if (gain > best_split->gain) {
-      best_split->reset_values(fx, value, gain, 
-                        bestP[le_idx], bestP[gt_idx]); 
+      best_split->reset_values(fx, value, gain,
+                        bestP[le_idx], bestP[gt_idx]);
     }
-#endif 
+#endif
   }
 }
 
@@ -215,27 +215,27 @@ void AzFindSplit::pick_split_points(int split_points_num,
   //AzCursor cursor; //@ A class can ++ --
   //sorted->rewind(cursor); //@cursor.set(0).In spares case it has backward option
   int total_data_num = sorted->dataNum();
-  int left_size = 0; 
+  int left_size = 0;
 
   for (int split_index = 0; split_index < split_points_num; split_index++) {
     double value = sorted->getValue(floor(total_data_num / double(split_points_num + 1)* (split_index + 1))); //@ The value of this threshold
     int index_num; //@ The number of exampls between two values
-    const int *index = NULL; 
+    const int *index = NULL;
     /*while ( left_size < total_data_num / double(split_points_num + 1)* (split_index + 1) )
     {//@only dense part changed?
       index = sorted->next(cursor, &value, &index_num); //@@@@@we should rewrite this
       left_size += index_num;
     } */
-    
+
     split_points[split_index] = value;
     //std::printf("pick_split_points:: No %d index %d value %f \n",split_index,left_size,value);
   }
 }
 
-void AzFindSplit::loop_on_given_points (AzTrTsplit *best_split, 
+void AzFindSplit::loop_on_given_points (AzTrTsplit *best_split,
                        int fx, /* feature# */
-                       const AzSortedFeat *sorted, 
-                       int total_size, 
+                       const AzSortedFeat *sorted,
+                       int total_size,
                        const Az_forFindSplit *total,
                        double *split_points,
                        int split_points_num,
@@ -243,20 +243,20 @@ void AzFindSplit::loop_on_given_points (AzTrTsplit *best_split,
 {
   /*---  first everyone is in GT(LE)  ---*/
   /*---  move the smallest(largest) ones from GT(LE) to LE(GT)  ---*/
-  const char *eyec = "AzFindSplit::loop_on_given_points"; 
-  int dest_size = 0; 
+  const char *eyec = "AzFindSplit::loop_on_given_points";
+  int dest_size = 0;
   Az_forFindSplit i[2];
-  Az_forFindSplit *src = &i[1], *dest = &i[0]; 
-  
-  double bestP[2] = {0,0}; 
-  int le_idx, gt_idx; 
+  Az_forFindSplit *src = &i[1], *dest = &i[0];
+
+  double bestP[2] = {0,0};
+  int le_idx, gt_idx;
   if (sorted->isForward()) {
-    le_idx = 0; 
-    gt_idx = 1; 
+    le_idx = 0;
+    gt_idx = 1;
   }
   else {
-    le_idx = 1; 
-    gt_idx = 0; 
+    le_idx = 1;
+    gt_idx = 0;
   }
 
   AzCursor cursor; //@ A class can ++ --
@@ -264,36 +264,36 @@ void AzFindSplit::loop_on_given_points (AzTrTsplit *best_split,
   double value = split_points[0] - 1;
   for (int split_index = 0; split_index < split_points_num; split_index++) {
     Az_forFindSplit *inf = &info[split_index*2];
-    Az_forFindSplit *src1 = &inf[1], *dest1 = &inf[0]; 
+    Az_forFindSplit *src1 = &inf[1], *dest1 = &inf[0];
 
      //@ The value of this threshold
     int index_num = 0; //@ The number of exampls between two values
-    const int *index = NULL; 
-    //@ try to find the first 
+    const int *index = NULL;
+    //@ try to find the first
     while ( value <= split_points[split_index] && cursor.get() < total_size )
     {//@only dense part changed?
       index = sorted->next_real(cursor, &value, &index_num); //@@@@@we should rewrite this
-      dest_size += index_num; 
+      dest_size += index_num;
       //if (index == NULL) break;
-    } 
+    }
     //@
-    //if (index == NULL)    std::cout<<"@ERRO:"<<eyec<<":dest_size="<<dest_size<<std::endl;  
+    //if (index == NULL)    std::cout<<"@ERRO:"<<eyec<<":dest_size="<<dest_size<<std::endl;
     if (dest_size >= total_size) {
-      //std::cout<<"@TEST:"<<eyec<<":ppdest_size="<<dest_size<<std::endl; 
+      //std::cout<<"@TEST:"<<eyec<<":ppdest_size="<<dest_size<<std::endl;
       //break; /* don't allow all vs nothing */
     }
 
-    //std::cout<<"@TEST:"<<eyec<<":dest_size="<<dest_size<<std::endl; 
+    //std::cout<<"@TEST:"<<eyec<<":dest_size="<<dest_size<<std::endl;
 
     const double *tarDw = target->tarDw_arr(); //@we can look this as target
     const double *dw = target->dw_arr(); //@we can look this as 1
-    double wy_sum_move = 0, w_sum_move = 0; 
-    int ix; 
+    double wy_sum_move = 0, w_sum_move = 0;
+    int ix;
     for (ix = 0; ix < index_num; ++ix) {
-      int dx = index[ix]; 
-      wy_sum_move += tarDw[dx]; 
-      w_sum_move += dw[dx]; 
-      //std::cout<<"@TEST:"<<eyec<<":dw[dx]="<<dw[dx]<<std::endl; 
+      int dx = index[ix];
+      wy_sum_move += tarDw[dx];
+      w_sum_move += dw[dx];
+      //std::cout<<"@TEST:"<<eyec<<":dw[dx]="<<dw[dx]<<std::endl;
     }
 
     dest->wy_sum += wy_sum_move; //@@@@@@@@Here here allreduce
@@ -306,16 +306,16 @@ void AzFindSplit::loop_on_given_points (AzTrTsplit *best_split,
     src->size = total_size  - dest_size;
 
     dest1->wy_sum = dest->wy_sum;
-    dest1->w_sum  = dest->w_sum; 
+    dest1->w_sum  = dest->w_sum;
     dest1->size = dest->size;
 
     src1->wy_sum = src->wy_sum;
-    src1->w_sum  = src->w_sum; 
+    src1->w_sum  = src->w_sum;
     src1->size = src->size;
-    
+
     /*if (min_size > 0) {
       if (dest_size < min_size) {
-        //continue; 
+        //continue;
       }
       if (total_size  - dest_size < min_size) {
         //break; //@very important for accuracy
@@ -325,15 +325,15 @@ void AzFindSplit::loop_on_given_points (AzTrTsplit *best_split,
 
     //@save i to make high level allreduce AND ALSO THE NLAM
     //
-    
+
 #if 0
 
     double gain = evalSplit(inf, bestP); //@@@we should rewrite this to make something allreduce
     if (gain > best_split->gain) {
-      best_split->reset_values(fx, split_points[split_index] , gain, 
-                        bestP[le_idx], bestP[gt_idx]); 
+      best_split->reset_values(fx, split_points[split_index] , gain,
+                        bestP[le_idx], bestP[gt_idx]);
     }
-#endif 
+#endif
     //*/
   }
 }
@@ -341,25 +341,24 @@ void AzFindSplit::loop_on_given_points (AzTrTsplit *best_split,
 void AzFindSplit::_pickFeats(int pick_num, int f_num)
 {
   if (pick_num < 1 || pick_num > f_num) {
-    throw new AzException("AzFindSplit::pickFeats", "out of range"); 
+    throw new AzException("AzFindSplit::pickFeats", "out of range");
   }
-  ia_feats.reset(); 
+  ia_feats.reset();
   if (pick_num == f_num) {
-    ia_fx = NULL; 
-    return; 
+    ia_fx = NULL;
+    return;
   }
 
-  AzIntArr ia_onOff; 
-  ia_onOff.reset(f_num, 0); 
-  int *onOff = ia_onOff.point_u(); 
+  AzIntArr ia_onOff;
+  ia_onOff.reset(f_num, 0);
+  int *onOff = ia_onOff.point_u();
   for ( ; ; ) {
-    if (ia_feats.size() >= pick_num) break; 
-    int fx = rand() % f_num; 
+    if (ia_feats.size() >= pick_num) break;
+    int fx = rand() % f_num;
     if (onOff[fx] == 0) {
-      onOff[fx] = 1; 
-      ia_feats.put(fx); 
+      onOff[fx] = 1;
+      ia_feats.put(fx);
     }
   }
-  ia_fx = &ia_feats; 
+  ia_fx = &ia_feats;
 }
- 
